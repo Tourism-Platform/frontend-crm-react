f2b1683aef052904a16f666b50b61868f48e2116
KDF25 <karimov.damir.faridovich@gmail.com>
2025-12-30 13:05:47 +0300

    fix: msw

--- src/app/init/msw.ts
+++ src/app/init/msw.ts
@@ -1,5 +1,7 @@
+import { ENV } from "@/shared/config";
+
 export const initMsw = async () => {
-	if (import.meta.env.DEV) {
+	if (import.meta.env.DEV || ENV.VITE_ENABLE_MSW === "true") {
 		const { worker } = await import("@/shared/api/msw/browser");
 		return worker.start({
 			onUnhandledRequest: "bypass"
--- src/shared/config/constants/env.config.ts
+++ src/shared/config/constants/env.config.ts
@@ -1,5 +1,7 @@
 const VITE_API_URL = import.meta.env.VITE_API_URL;
+const VITE_ENABLE_MSW = import.meta.env.VITE_ENABLE_MSW;
 
 export const ENV = {
-	VITE_API_URL
+	VITE_API_URL,
+	VITE_ENABLE_MSW
 };
