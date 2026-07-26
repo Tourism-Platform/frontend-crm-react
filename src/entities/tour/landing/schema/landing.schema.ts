decf28dda06128a656c8ddabfd60b27be3b16e9f
KDF25 <karimov.damir.faridovich@gmail.com>
2026-07-26 21:52:32 +0500

    feat: 5000 smbls

--- src/entities/tour/landing/schema/landing.schema.ts
+++ src/entities/tour/landing/schema/landing.schema.ts
@@ -30,14 +30,14 @@ export const LANDING_SCHEMA = z.object({
 	[ENUM_FORM_LANDING.PICKUP_DESCRIPTION]: z
 		.string()
 		.trim()
-		.max(1000, {
+		.max(5000, {
 			message: msg("form.pickup.fields.pickup_description.errors.max")
 		})
 		.optional(),
 	[ENUM_FORM_LANDING.CANCELLATION_POLICY]: z
 		.string()
 		.trim()
-		.max(2000, {
+		.max(5000, {
 			message: msg(
 				"form.cancellation.fields.cancellation_policy.errors.max"
 			)
@@ -46,7 +46,7 @@ export const LANDING_SCHEMA = z.object({
 	[ENUM_FORM_LANDING.ADDITIONAL_INFO]: z
 		.string()
 		.trim()
-		.max(2000, {
+		.max(5000, {
 			message: msg(
 				"form.additional_info.fields.additional_info.errors.max"
 			)
