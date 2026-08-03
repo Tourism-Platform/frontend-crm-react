14771191e09d1997d85752e7af7e384947fd5783
KDF25 <karimov.damir.faridovich@gmail.com>
2026-08-03 14:11:25 +0500

    fix: rename dir finance

--- src/pages/booking/order-id-page/ui/order-id-page.tsx
+++ src/pages/booking/order-id-page/ui/order-id-page.tsx
@@ -1,6 +1,6 @@
 import { type FC } from "react";
 
-import { OrderId } from "@/widgets/booking";
+import { OrderId } from "@/widgets/booking-operator";
 
 export const OrderIdPage: FC = () => {
 	return <OrderId />;
--- src/pages/booking/orders-page/ui/orders-page.tsx
+++ src/pages/booking/orders-page/ui/orders-page.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 
-import { Orders } from "@/widgets/booking";
+import { Orders } from "@/widgets/booking-operator";
 
 export const OrdersPage: FC = () => {
 	return <Orders />;
